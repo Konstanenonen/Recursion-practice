8bc480f686e14079d8e1e5fd362fb4f1e95fd312
Konsta Nenonen <konstanenonen@protonmail.com>
2022-10-12 00:04:36 +0300

    Added the recursive variation

--- fibonacci.js
+++ fibonacci.js
@@ -14,3 +14,10 @@ function fibs(num) {
 
   return fibArr;
 }
+
+function fib(n) {
+  if (n == 0) return [0];
+  if (n == 1) return [0, 1];
+  const arr = fib(n - 1);
+  return [...arr, arr[n - 1] + arr[n - 2]];
+}
